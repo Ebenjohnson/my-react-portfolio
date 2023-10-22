d4d9e46c2a7de8dc56c1a598dbb65534f1ff9b32
Ebenjohnson <ebenezer.johnson@outlook.com>
2023-10-22 11:28:44 -0700

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react"
+import Navbar from "./scenes/Navbar"
+
 
 
 function App() {
-  //const [count, setCount] = useState(0)
+
+  const [selectedPage, setSelectedPage] = useState('home')
+  const [isTopOfPage, setTopOfPage] = useState('(min-width:1060px)')
 
 
 
-  return (
-    <>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return  <div className="app bg-deep-blue">
+    <Navbar>
+      selectedPage ={selectedPage} setSelectedPage={setSelectedPage}
+    </Navbar>
+  </div>
+ // (
+  //   <>
+
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
 }
 
 export default App
